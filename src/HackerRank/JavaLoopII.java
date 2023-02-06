807057a5cacdaf2311a31962245e08f3bc9b010c
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-06 07:23:08 +0530

    JavaLoopII

---
+++ src/HackerRank/JavaLoopII.java
@@ -0,0 +1,27 @@
+package HackerRank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class JavaLoopII {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+//String[] input = new String[3];
+//int a;
+//int b;
+//int n;
+//
+//        System.out.println("Enter a,b and n values: ");
+//        input = in.readLine().split("");
+
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Integers: ");
+int a = scanner.nextInt();
+int b = scanner.nextInt();
+
+        System.out.println("Integers are " + a + " " + b);
+    }
+}

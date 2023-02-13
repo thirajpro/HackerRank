27fffc5ed392c67130b5f12949e1ebb09dd1861f
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-13 14:51:40 +0530

    Solved Data Types

--- src/HackerRank/DataTypes.java
+++ src/HackerRank/DataTypes.java
@@ -8,14 +8,31 @@ import static java.lang.Math.pow;
 public class DataTypes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i1 = 4;
-        double[] input = new double[4];
-
-        for (int i = 0; i < input.length; i++) {
-            input[i] = scanner.nextInt();
+        for(int i = 0;i<5;i++){
+            long input = scanner.nextLong();
+            if (input<(Math.pow(2,8)-1) && input> -Math.pow(2,7)){
+                System.out.println(input + " can be fitted in :");
+                System.out.println("* byte");
+                System.out.println("*Short");
+                System.out.println("*int");
+                System.out.println("*long");
+            } else if (input<(Math.pow(2,16)-1) && input> -Math.pow(2,16)){
+                    System.out.println(input + " can be fitted in :");
+                    System.out.println("*Short");
+                    System.out.println("*int");
+                    System.out.println("*long");
+            }else if (input<(Math.pow(2,31)-1) && input> -Math.pow(2,31)) {
+                System.out.println(input + " can be fitted in :");
+                System.out.println("*int");
+                System.out.println("*long");
+            }else if (input<(Math.pow(2,63)-1) && input> -Math.pow(2,63)) {
+                System.out.println(input + " can be fitted in :");
+                System.out.println("*long");
+            }
+            else {
+                System.out.println(input + " can't be fitted in anywhere");
+            }
         }
-        System.out.println(Arrays.toString(input));
-
     }
 }
 

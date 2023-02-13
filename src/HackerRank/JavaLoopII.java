39206866be124c67319fa2ff78e11764ed19cb68
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-13 11:54:59 +0530

    Java Loop II Solved

--- src/HackerRank/JavaLoopII.java
+++ src/HackerRank/JavaLoopII.java
@@ -10,15 +10,19 @@ import java.util.Scanner;
 
 public class JavaLoopII {
     public static void main(String[] args) throws IOException {
-    Scanner scanner = new Scanner(System.in);
-   int a = scanner.nextInt();
-   int b = scanner.nextInt();
-   int n = scanner.nextInt();
-   int output = a;
-   for (int i = 1;i<= n;i++){
-       int c = (int)Math.pow(2,i-1);
-        output = output+ (c*b);
-       System.out.print(output + " ");
-   }
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+        for (int j = 1; j <= q; j++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+            int output = a;
+            for (int i = 1; i <= n; i++) {
+                int c = (int) Math.pow(2, i - 1);
+                output = output + (c * b);
+                System.out.print(output + " ");
+            }
+            System.out.println();
+        }
     }
 }

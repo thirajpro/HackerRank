777cbf0a8a498d81955e363503207ee24e550720
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-20 20:27:08 +0530

    IntToString Solved

--- src/HackerRank/DataTypes.java
+++ src/HackerRank/DataTypes.java
@@ -1,5 +1,6 @@
 package HackerRank;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,31 +9,33 @@ import static java.lang.Math.pow;
 public class DataTypes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for(int i = 0;i<5;i++){
-            long input = scanner.nextLong();
-            if (input<(Math.pow(2,8)-1) && input> -Math.pow(2,7)){
+        for (int i = 0; i < 5; i++) {
+            float input = scanner.nextFloat();
+            if (input < (Math.pow(2, 8) - 1) && input > -Math.pow(2, 7)) {
                 System.out.println(input + " can be fitted in :");
-                System.out.println("* byte");
+                System.out.println("*byte");
                 System.out.println("*Short");
                 System.out.println("*int");
                 System.out.println("*long");
-            } else if (input<(Math.pow(2,16)-1) && input> -Math.pow(2,16)){
-                    System.out.println(input + " can be fitted in :");
-                    System.out.println("*Short");
-                    System.out.println("*int");
-                    System.out.println("*long");
-            }else if (input<(Math.pow(2,31)-1) && input> -Math.pow(2,31)) {
+            } else if (input < (Math.pow(2, 16) - 1) && input > -Math.pow(2, 16)) {
+                System.out.println(input + " can be fitted in :");
+                System.out.println("*Short");
+                System.out.println("*int");
+                System.out.println("*long");
+            } else if (input < (Math.pow(2, 31) - 1) && input > -Math.pow(2, 31)) {
                 System.out.println(input + " can be fitted in :");
                 System.out.println("*int");
                 System.out.println("*long");
             }else if (input<(Math.pow(2,63)-1) && input> -Math.pow(2,63)) {
                 System.out.println(input + " can be fitted in :");
                 System.out.println("*long");
-            }
-            else {
+            } else {
                 System.out.println(input + " can't be fitted in anywhere");
+
             }
         }
     }
 }
+
+
 

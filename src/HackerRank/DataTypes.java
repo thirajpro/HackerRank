0f0eba58b7707014f7ead5b4c9fbef7137c69b2d
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-23 13:23:10 +0530

    Solved Data Types

--- src/HackerRank/DataTypes.java
+++ src/HackerRank/DataTypes.java
@@ -8,33 +8,37 @@ import static java.lang.Math.pow;
 
 public class DataTypes {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            float input = scanner.nextFloat();
-            if (input < (Math.pow(2, 8) - 1) && input > -Math.pow(2, 7)) {
-                System.out.println(input + " can be fitted in :");
-                System.out.println("*byte");
-                System.out.println("*Short");
-                System.out.println("*int");
-                System.out.println("*long");
-            } else if (input < (Math.pow(2, 16) - 1) && input > -Math.pow(2, 16)) {
-                System.out.println(input + " can be fitted in :");
-                System.out.println("*Short");
-                System.out.println("*int");
-                System.out.println("*long");
-            } else if (input < (Math.pow(2, 31) - 1) && input > -Math.pow(2, 31)) {
-                System.out.println(input + " can be fitted in :");
-                System.out.println("*int");
-                System.out.println("*long");
-            }else if (input<(Math.pow(2,63)-1) && input> -Math.pow(2,63)) {
-                System.out.println(input + " can be fitted in :");
-                System.out.println("*long");
-            } else {
-                System.out.println(input + " can't be fitted in anywhere");
 
+            Scanner sc = new Scanner(System.in);
+            int t=sc.nextInt();
+
+            for(int i=0;i<t;i++) {
+                try {
+                    long x=sc.nextLong();
+                    System.out.println(x+" can be fitted in:");
+                    if(x>=-128 && x<=127){
+                        System.out.println("* byte");
+                        System.out.println("* short");
+                        System.out.println("* int");
+                        System.out.println("* long");
+                    }else if (x>=-32768 && x<= 32767) {
+                        System.out.println("* short");
+                        System.out.println("* int");
+                        System.out.println("* long");
+                    } else if (x>=-Math.pow(2,31) && x<= Math.pow(2,31)-1){
+                        System.out.println("* int");
+                        System.out.println("* long");
+                    }else if (x>=-Math.pow(2,64) && x<=Math.pow(2,64)){
+                        System.out.println("* long");
+                    }
+                }
+                //Complete the code
+                catch(Exception e)
+                {
+                    System.out.println(sc.next()+" can't be fitted anywhere.");
+                }
             }
         }
-    }
 }
 
 
